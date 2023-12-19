4a6411f52474cc4ef065b4c362d87ab197efccf9
Hynarjalasutram <hynaaz.j@gmail.com>
2023-12-19 19:11:35 +0530

    Updates

---
+++ precache-manifest.b096239962ab9fd85913952affd3534b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efc9c2a1a8e3dff46431fbe91374186",
+    "url": "/Aboutme/index.html"
+  },
+  {
+    "revision": "a0d658f67c50272a9fd5",
+    "url": "/Aboutme/static/css/main.4e672b95.chunk.css"
+  },
+  {
+    "revision": "9d3c055cae0d9cfee657",
+    "url": "/Aboutme/static/js/2.472276fb.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "/Aboutme/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "a0d658f67c50272a9fd5",
+    "url": "/Aboutme/static/js/main.cb97380c.chunk.js"
+  },
+  {
+    "revision": "4c76f92a63a887049f4d",
+    "url": "/Aboutme/static/js/runtime~main.bb541c0b.js"
+  },
+  {
+    "revision": "e572de67821dd709a0170a993b3d41cf",
+    "url": "/Aboutme/static/media/Expressjs.e572de67.jpg"
+  },
+  {
+    "revision": "fdea1a584289031d5ed37e9534ee9ae3",
+    "url": "/Aboutme/static/media/LinkedInclone.fdea1a58.png"
+  },
+  {
+    "revision": "7f2e99de64344809949615c122230d7d",
+    "url": "/Aboutme/static/media/PHOTO.7f2e99de.jpg"
+  },
+  {
+    "revision": "3f503e4b1591bef0268d75df3713bdcb",
+    "url": "/Aboutme/static/media/R.3f503e4b.png"
+  },
+  {
+    "revision": "0d9fd48869d05b68cb5c371056cf6af7",
+    "url": "/Aboutme/static/media/Sampleclone.0d9fd488.png"
+  },
+  {
+    "revision": "cf188899b6f360d88ac4b2bee4443561",
+    "url": "/Aboutme/static/media/Vclone.cf188899.png"
+  },
+  {
+    "revision": "0d8bb8ce98bb0b7f58b9eb077dcc6f8d",
+    "url": "/Aboutme/static/media/Vclone1.0d8bb8ce.png"
+  },
+  {
+    "revision": "590ccaba9d6c8aa9c7dd15bfbc2cbd6e",
+    "url": "/Aboutme/static/media/WebApp.590ccaba.png"
+  },
+  {
+    "revision": "003808cd824794b107adf4b9f5b36ca5",
+    "url": "/Aboutme/static/media/mamaearthclone.003808cd.png"
+  },
+  {
+    "revision": "32d90055556d6909417de55f7a313ecb",
+    "url": "/Aboutme/static/media/postman.32d90055.png"
+  },
+  {
+    "revision": "652e9ca1114f1169324be088d5bb5f65",
+    "url": "/Aboutme/static/media/sql.652e9ca1.png"
+  }
+]);
