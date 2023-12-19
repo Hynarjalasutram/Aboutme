cb4c6a379fff00d4972e421ec74b03b9f49c989f
Hynarjalasutram <84487766+Hynarjalasutram@users.noreply.github.com>
2023-12-19 19:20:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ let App=()=>{
     <BrowserRouter>
     <Nav/>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/Aboutme/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/skills" element={<Skills/>}/>
       <Route path="/projects" element={<Projects/>}/>
